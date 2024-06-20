905f4dc7cf1f1b4c1bd14d1057354044a0f8bf0f
KFerrei <116793771+KFerrei@users.noreply.github.com>
2024-06-20 21:05:22 +0200

    Initial Commit

---
+++ GreenBond/GreenBondApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenBondApp.swift
+//  GreenBond
+//
+//  Created by FERREIRA Kévin on 20/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct GreenBondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
